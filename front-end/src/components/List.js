e8e884716103778cbf205cc6ed5a02d64c60ef94
Imesh Senanayake <imeshsenanayake1996@gmail.com>
2023-01-31 17:45:27 +0530

    Update List.js
    2023/01/31

--- front-end/src/components/List.js
+++ front-end/src/components/List.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function List() {
+  const[product,setProduct] = useState([]);
+  useEffect(()=>{
+    fetch("http://localhost:8080/api/products/")
+    .then(res=>res.json())
+    .then((result)=>{
+      setProduct(result);
+    },[])
+  }
+  )
   return (
     <div>
-      list
+      {product.map(products=>(
+        <p key={products.name}>
+          Name:{products.name}<br/>
+          Description:{products.description}<br/>
+          <button>Delete</button>
+        </p>
+      )
+      )}
     </div>
   )
 }
